9584e660db59d28547303d6a2520a88c316c9a77
Agustin Mamani <agusdmu1999@gmail.com>
2024-07-13 12:54:52 -0500

    agregando records

---
+++ src/main/java/com/alura/literalura/model/DatosAutor.java
@@ -0,0 +1,13 @@
+package com.alura.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutor(
+        //Json alias nos permite leer y con jsonproperty nos permite leer y escribir
+        @JsonAlias("name") String nombreAutor,
+        @JsonAlias("birth_year") String añoNac,
+        @JsonAlias("death_year") String añoDef
+) {
+}
---
+++ src/main/java/com/alura/literalura/model/DatosLibro.java
@@ -0,0 +1,17 @@
+package com.alura.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibro(
+        //Json alias nos permite leer y con jsonproperty nos permite leer y escribir
+        @JsonAlias("Id") Long id,
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") List<DatosAutor> autores,
+        @JsonAlias("languages") List<String> lenguaje
+) {
+
+}
