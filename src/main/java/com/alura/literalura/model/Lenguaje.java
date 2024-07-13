505b0f992bde7d3fce9a1327a0ac0ed0c642bd74
Agustin Mamani <agusdmu1999@gmail.com>
2024-07-13 12:55:00 -0500

    agregando enum lenguaje

---
+++ src/main/java/com/alura/literalura/model/Lenguaje.java
@@ -0,0 +1,24 @@
+package com.alura.literalura.model;
+
+public enum Lenguaje {
+    ESPAÑOL("es","español"),
+    INGLES("en", "ingles"),;
+
+    private String lenguajeGutendex;
+    private String lenguajeEspañol;
+
+    Lenguaje(String lenguajeGutendex, String lenguajeEspañol){
+        this.lenguajeEspañol = lenguajeEspañol;
+        this.lenguajeGutendex = lenguajeGutendex;
+    }
+    //Convertir la categoria a string
+    public static Lenguaje fromString(String text) {
+        for (Lenguaje lenguaje: Lenguaje.values()){
+            if (lenguaje.lenguajeGutendex.equalsIgnoreCase(text) || lenguaje.lenguajeEspañol.equalsIgnoreCase(text)){
+                return lenguaje;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: "+ text);
+    }
+
+}
